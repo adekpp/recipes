01e763d7f26c30f5af443466f943413c2f14ae1e
adekpp <adekpp@gmail.com>
2022-10-12 21:09:46 +0200

    changed env

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ussmhjyywpndzbloaezb.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
