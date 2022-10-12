2b05961f95216f27aa07fb115ee973de4e6e347e
adekpp <adekpp@gmail.com>
2022-10-12 21:17:27 +0200

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-export default ({ mode }) => {
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
-  return defineConfig({
-    plugins: [vue()],
-  });
-};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+});
