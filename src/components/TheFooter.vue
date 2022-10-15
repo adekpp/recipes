0a0e6453e4684bc0d8a67099ebb3b342ad7a5e06
adekpp <adekpp@gmail.com>
2022-10-15 13:20:19 +0200

    footer link updates

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,32 +1,39 @@
 <script setup></script>
 <template>
   <div class="w-full bg-[#F9F9F9] text-gray-900 max-w-full px-10 mx-auto -mb-1">
-    
-      <div class="grid grid-cols-1 md:grid-cols-2 place-items-center">
-        <div
-          class="grid grid-rows-2 max-w-[400px] mb-4 md:mb-0 place-items-center md:place-items-start"
+    <div class="grid grid-cols-1 md:grid-cols-2 place-items-center">
+      <div
+        class="grid grid-rows-2 max-w-[400px] mb-4 md:mb-0 place-items-center md:place-items-start"
+      >
+        <h1
+          class="font-Playball font-semibold text-4xl text-gray-900 mb-2 mt-4"
         >
-          <h1
-            class="font-Playball font-semibold text-4xl text-gray-900 mb-2 mt-4"
-          >
-            Delicious Recipes
-          </h1>
-          <p class="italic text-sm">
-            The site was created for the purpose of showcasing my skills. It
-            does not have full functionality. I hope you like it.
-          </p>
-        </div>
+          Delicious Recipes
+        </h1>
+        <p class="italic text-sm">
+          The site was created for the purpose of showcasing my skills. It does
+          not have full functionality. I hope you like it.
+        </p>
+      </div>
 
-        <div class="grid grid-cols-3 place-items-center gap-4">
-          <p>GitHub</p>
-          <p>Portfolio</p>
-          <p>LinkedIn</p>
-        </div>
+      <div class="grid grid-cols-3 place-items-center gap-4">
+        <p><a href="https://github.com/adekpp" target="blank">GitHub</a></p>
+        <p>
+          <a href="https://adrian-pietryga.netlify.app" target="blank"
+            >Portfolio</a
+          >
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/adrian-pietryga" target="blank"
+            >LinkedIn</a
+          >
+        </p>
       </div>
-      <div class="w-full flex place-content-center mt-3 mb-2">
-        <small class="italic text-center">2022 | Adrian Pietryga</small>
-      </div>
-   
+    </div>
+
+    <div class="w-full flex place-content-center mt-3 mb-2">
+      <small class="italic text-center">2022 | Adrian Pietryga</small>
+    </div>
   </div>
 </template>
 <style scoped></style>
