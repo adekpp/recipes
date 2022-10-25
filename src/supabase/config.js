a67c5df3c854f7b61bbe363b697340791cdfbe2c
Adrian Pietryga <36409418+adekpp@users.noreply.github.com>
2022-10-25 23:03:18 +0200

    Update config.js

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ussmhjyywpndzbloaezb.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzc21oanl5d3BuZHpibG9hZXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMxODQyMDMsImV4cCI6MTk3ODc2MDIwM30.IhtBR3q9f0yx0f2_4qjmjiQUgB7moU9TlfAyAzvaxxY
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey);
